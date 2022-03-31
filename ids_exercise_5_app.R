7d9029a9b2b9954c40714ef51ad2e276cbd67c24
foriana <joyf.peterson@gmail.com>
2022-03-31 17:28:35 -0500

    Commit libraries

---
+++ ids_exercise_5_app.R
@@ -0,0 +1,21 @@
+title: 'Weekly Exercise #5, Problem #7'
+author: "Felicia Peterson"
+
+library(tidyverse)     # for data cleaning and plotting
+library(gardenR)       # for Lisa's garden data
+library(lubridate)     # for date manipulation
+library(openintro)     # for the abbr2state() function
+library(palmerpenguins)# for Palmer penguin data
+library(maps)          # for map data
+library(ggmap)         # for mapping points on maps
+library(gplots)        # for col2hex() function
+library(RColorBrewer)  # for color palettes
+library(sf)            # for working with spatial data
+library(leaflet)       # for highly customizable mapping
+library(ggthemes)      # for more themes (including theme_map())
+library(plotly)        # for the ggplotly() - basic interactivity
+library(gganimate)     # for adding animation layers to ggplots
+library(transformr)    # for "tweening" (gganimate)
+library(gifski)        # need the library for creating gifs but don't need to load each time
+library(shiny)         # for creating interactive apps
+
