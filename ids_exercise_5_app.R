87d9d489bf24a8531180863db42694c1924952de
foriana <joyf.peterson@gmail.com>
2022-04-03 16:17:24 -0500

    Commit .r

--- ids_exercise_5_app.R
+++ ids_exercise_5_app.R
@@ -1,4 +1,11 @@
-
+#You will create an app to compare states’ daily number of COVID cases per 100,000 over time. 
+#The x-axis will be date. 
+#You will have an input box where the user can choose which states to compare (selectInput()), 
+#a slider where the user can choose the date range, 
+#and a submit button to click once the user has chosen all states they’re interested in comparing.
+#The graph should display a different line for each state, 
+#with labels either on the graph or in a legend. 
+#Color can be used if needed.
 
 library(tidyverse)     # for data cleaning and plotting
 library(gardenR)       # for Lisa's garden data
@@ -17,7 +24,12 @@ library(gganimate)     # for adding animation layers to ggplots
 library(transformr)    # for "tweening" (gganimate)
 library(gifski)        # need the library for creating gifs but don't need to load each time
 library(shiny)         # for creating interactive apps
-library(shiny)
-ui <- fluidPage("United States’ daily number of COVID cases per 100,000 over time")
+
+ui <- fluidPage("United States’ daily number of COVID cases per 100,000 over time"), 
+selectInput(inputId = "year",
+            label = "Year", 
+            value = "",
+            placeholder = "2019"),
+submitButtom(text = "Submit")
 server <- function(input, output) {}
 shinyApp(ui = ui, server = server)
