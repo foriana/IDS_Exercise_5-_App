1b7c56891577efa5061ea5e4c965d5cbde45025d
foriana <joyf.peterson@gmail.com>
2022-04-04 17:17:22 -0500

    commit .r

--- ids_exercise_5_app.R
+++ ids_exercise_5_app.R
@@ -9,31 +9,20 @@
 
 library(tidyverse)     # for data cleaning and plotting
 library(gardenR)       # for Lisa's garden data
-library(lubridate)     # for date manipulation
-library(openintro)     # for the abbr2state() function
-library(palmerpenguins)# for Palmer penguin data
-library(maps)          # for map data
-library(ggmap)         # for mapping points on maps
-library(gplots)        # for col2hex() function
-library(RColorBrewer)  # for color palettes
-library(sf)            # for working with spatial data
-library(leaflet)       # for highly customizable mapping
-library(ggthemes)      # for more themes (including theme_map())
-library(plotly)        # for the ggplotly() - basic interactivity
-library(gganimate)     # for adding animation layers to ggplots
+library(lubridate)     # for date manipulation    # for adding animation layers to ggplots
 library(transformr)    # for "tweening" (gganimate)
 library(gifski)        # need the library for creating gifs but don't need to load each time
 library(shiny)         # for creating interactive apps
-
-ui <- fluidPage("United States’ daily number of COVID cases per 100,000 over time"), 
+ui <- fluidPage("United States’ daily number of COVID cases per 100,000 over time")
 selectInput("state",
             "State",
-            choices = list(state)),
+            choices = list(state))
 sliderInput(inputId = "year",
             label = "Year Range", 
             min = 2019,
             max = 2022,
-            sep = ""),
+            sep = "")
 submitButtom(text = "Submit")
-server <- function(input, output) {}
+server <- function(input, output) {covid19 <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+}
 shinyApp(ui = ui, server = server)
